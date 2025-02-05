56dee5df4a6c1b07bbb4a71747c4c45d5b7c8d9c
mubashir12-a <ma2625645@gmail.com>
2025-02-05 20:41:09 +0530

    Updated On 05-02-2025

---
+++ ExternalJavaScript/Gsap.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let timeLn = gsap.timeline();
+
+    timeLn.from("#title", {
+        x: -300,
+        opacity: 0,
+        duration: 1
+    })
+
+    timeLn.from(".Al", {
+        duration: 1,
+        y: -300,  // From the left
+        opacity: 0,
+        stagger: 0.3,
+    })
+
+    gsap.from(".NavEl", {
+        x: 500,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.3
+    })
+
+    gsap.from(".introContent", {
+        x: -300,
+        opacity: 0,
+        duration: 1
+    })
+
+    gsap.from(".img", {
+        x: 300,
+        duration: 1,
+        opacity: 0
+    })
+
+    const tl = gsap.timeline();
+
+    tl.from("#icons img", {
+        y: 300,
+        duration: 1.5,
+        opacity: 0,
+        stagger: 0.3
+    });
+    
+    // Continuous rotation animation (after initial animation completes)
+    tl.to("#icons img:nth-child(odd)", {
+        rotate: 30,
+        duration: 1.5,
+        repeat: -1,
+        yoyo: true
+    }, "-=1.5"); // Overlapping to make it smoother
+    
+    tl.to("#icons img:nth-child(even)", {
+        rotate: -30,
+        duration: 1.5,
+        repeat: -1,
+        yoyo: true
+    }, "-=1.5");
+})
