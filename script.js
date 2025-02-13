ba284aa8dd9677c3b6dd3fa2bfc5704b2f2c4a19
mubashir12-a <ma2625645@gmail.com>
2025-02-13 22:54:50 +0530

    Updated On 13-02-2025

--- script.js
+++ script.js
@@ -7,33 +7,42 @@
 let spanArr = document.querySelectorAll(".container .block span");
 
 let ObjLang = {
-    "HTML" : 80,
-    "CSS" : 75,
-    "Bootstrap" : 60,
-    "SASS" : 50,
-    "JS" : 40,
-    "MySQL" : 45,
-    "PHP" : 45,
-    "JAVA" : 40,
-    "C" : 30,
-    "Python" : 20,
-    "GSAP" : 20,
-    "React" : 10
+    "HTML": 80,
+    "CSS": 75,
+    "Bootstrap": 60,
+    "SASS": 50,
+    "JS": 40,
+    "MySQL": 45,
+    "PHP": 45,
+    "JAVA": 40,
+    "C": 30,
+    "Python": 20,
+    "GSAP": 20,
+    "React": 10
 }
 
 let Keys = Object.keys(ObjLang);
 let i = 0;
 
-function SetSkillArc(i, per){
+function SetSkillArc(i, per) {
     let j = 1;
     let Interval = setInterval(() => {
-        if(j <= per){
+        if (j <= per) {
             spanArr[i].parentNode.style.background = `conic-gradient(cyan 0% ${j}%, transparent ${j}% 100%)`;
             spanArr[i].textContent = j + "%";
             j++;
-        } else{
+        } else {
             clearInterval(Interval);
         }
     }, 50)
 }
+
+
+// Send Message:
+
+let BtnMsg = document.getElementById("SndMsg");
+
+BtnMsg.addEventListener('click', () => {
+   
+})
 
